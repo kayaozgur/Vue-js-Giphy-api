ea98a4ac3637975926819b0a3ea8c5f63985d1b0
Özgür Kaya <okaya.gm@gmail.com>
2018-03-27 10:51:38 +0300

    tamamlandı

--- main.js
+++ main.js
@@ -2,13 +2,28 @@ var app = new Vue({
 
     el: '#app',
     data: {
-        liste: []
+        inputText: '',
+        liste: [],
+        isLoading: true
+    },
+
+    methods: {
+        ara(e) {
+            e.preventDefault();
+            this.liste = [];
+            this.isLoading = true;
+            const url = `http://api.giphy.com/v1/gifs/search?q=${this.inputText}&api_key=dc6zaTOxFJmzC`;
+            fetch(url).then((res) => { return res.json() })
+                .then((res) => { this.liste = res.data })
+            this.isLoading = false;
+        }
     },
 
     created() {
         const url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
         fetch(url).then((res) => { return res.json() })
-        .then((res) => { this.liste = res.data})
+            .then((res) => { this.liste = res.data })
+        this.isLoading = false;
 
 
     }
